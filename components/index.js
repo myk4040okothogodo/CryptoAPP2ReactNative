7858cacc50f782b09722c60a31105d0b5cb3a4f7
myk4040okothogodo <mikeogodo5@gmail.com>
2022-02-06 16:04:47 +0300

    cryptodetailfinal

--- components/index.js
+++ components/index.js
@@ -1,11 +1,13 @@
 import PriceAlert from "./PriceAlert";
 import TransactionHistory from  "./TransactionHistory";
 import HeaderBar from "./HeaderBar";
-import CurrencyLabel from "./CurrencyLabel"
+import CurrencyLabel from "./CurrencyLabel";
+import TextButton  from "./TextButton";
 
 export {
    PriceAlert,
    TransactionHistory,
    HeaderBar,
-   CurrencyLabel
+   CurrencyLabel,
+   TextButton
 }
